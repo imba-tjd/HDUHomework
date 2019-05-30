de2a2c75c351a1ff795544d682266f3be82e04d5
imba-tjd <109224573@qq.com>
2019-05-30 10:19:57 +0800

    create getmd5 header

---
+++ InformationSecurityTechnology/MD5/getmd5.h
@@ -0,0 +1,6 @@
+#ifndef __GETMD5_H__
+#define __GETMD5_H__
+
+void getmd5(FILE *f, char dest[32]);
+
+#endif
--- InformationSecurityTechnology/MD5/main.c
+++ InformationSecurityTechnology/MD5/main.c
@@ -1,31 +1,13 @@
-#include "md5.h"
-#include <assert.h>
 #include <stdio.h>
-#include <string.h>
-
-void getmd5(FILE *f, char dest[32])
-{
-    assert(f != NULL);
-
-    md5_state_t state;
-    md5_byte_t digest[16];
-    char buffer[448];
-    size_t cnt;
-
-    md5_init(&state);
-    while ((cnt = fread(buffer, sizeof(char), 448, f)) != 0)
-        md5_append(&state, (const md5_byte_t *)buffer, cnt);
-    md5_finish(&state, digest);
-
-    for (int di = 0; di < 16; ++di)
-        sprintf(dest + di * 2, "%02x", digest[di]);
-}
+#include "getmd5.h"
 
 int main()
 {
     char md5[33];
     FILE *f = fopen("plain.txt", "rb");
-    assert(f != NULL);
+    if(f == NULL)
+        return -1;
+
     getmd5(f, md5);
     md5[32] = '\0';
     puts(md5);
